df7de1f72f0156318adb8106d094e59998499866
Binay Mishra <binaymishra@gmail.com>
2016-10-17 17:56:24 +0530

    Returning only status code 200 OK

--- src/main/java/com/expense/item/service/Application.java
+++ src/main/java/com/expense/item/service/Application.java
@@ -2,19 +2,18 @@ package com.expense.item.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @EnableAutoConfiguration
 public class Application {
 
 	@RequestMapping("/")
-	@ResponseBody
-	public String home() {
-		return "' expense-item-service ' is up and running, requested URL [/] and response HTTP 200 OK.";
-	}
+	@ResponseStatus(value = HttpStatus.OK)
+	public void home() {	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
