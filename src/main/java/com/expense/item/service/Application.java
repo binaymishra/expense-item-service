4fe5b0cffef44d7f2078dec1e9008b30b652fcec
Binay Mishra <binaymishra@gmail.com>
2016-10-17 17:47:55 +0530

    Returining response.

--- src/main/java/com/expense/item/service/Application.java
+++ src/main/java/com/expense/item/service/Application.java
@@ -2,19 +2,18 @@ package com.expense.item.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 public class Application {
 
 	@RequestMapping("/")
-	@ResponseStatus(value = HttpStatus.OK)
-	public void home() {
-		System.out.println("' expense-item-service ' is up and running, requested URL [/] and response HTTP 200 OK.");
+	@ResponseBody
+	public String home() {
+		return "' expense-item-service ' is up and running, requested URL [/] and response HTTP 200 OK.";
 	}
 
 	public static void main(String[] args) {
