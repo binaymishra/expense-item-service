efed0a7d7d8cb01122cf6fb0f82f296d86b8008a
Binay Mishra <binaymishra@gmail.com>
2016-10-18 06:43:23 +0530

    expense data file repository bean configuration

--- src/main/java/com/expense/item/service/ApplicationConfiguration.java
+++ src/main/java/com/expense/item/service/ApplicationConfiguration.java
@@ -1,10 +1,18 @@
 package com.expense.item.service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {"com.expense.item.service.controller"})
 public class ApplicationConfiguration {
+	
+	@Bean(initMethod="loadItems")
+	public DataLoader loadQuoteData(){
+		DataLoader dataLoader = new DataLoader();
+		dataLoader.setFilePath("classpath:items.data");
+		return dataLoader;
+	}
 
 }
---
+++ src/main/java/com/expense/item/service/controller/ExpenseItemController.java
@@ -0,0 +1,39 @@
+package com.expense.item.service.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.expense.item.service.DataLoader;
+import com.expense.model.ItemServiceResponse;
+
+@Controller
+public class ExpenseItemController {
+	
+	@Autowired
+	private DataLoader dataLoader;
+	
+	@RequestMapping("/expense/item")
+	@ResponseBody
+	ItemServiceResponse findAllItems(){
+		ItemServiceResponse response = new ItemServiceResponse();
+		response.setItemIdNameMap(prepareItemMap(dataLoader.loadItems()));
+		return response;
+	}
+	
+	private Map<Integer, String> prepareItemMap(List<String> items){
+		Map<Integer, String> map = new HashMap<>(items.size());
+		for(String item: items){
+			String[] split = StringUtils.split(item, "|");
+			map.put(Integer.parseInt(StringUtils.trim(split[0])), StringUtils.trim(split[1]));
+		}
+		return map;
+	}
+
+}
